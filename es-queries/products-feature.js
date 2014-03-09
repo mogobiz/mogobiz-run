501ea3170f65b4d8e636f46b524e0ad4a1db2358
cgalant <christophe.galant@gmail.com>
2014-03-09 20:27:31 +0100

    - finalisation des requetes ES (2)

--- es-queries/products-feature.js
+++ es-queries/products-feature.js
@@ -1,5 +1,99 @@
-// Statut : en attente d'info
+// Statut : OK
 // StoreController
 //REMARQUE : idem que products avec un query parameter featured supplémentaire
 restpath = "/api/store/{storeCode}/products?lang=fr&currency={curCode}&country={countryCode}&featured=true"
 method = "POST" // et/ou "GET"
+
+
+
+method="POST"
+curl = "http://localhost:9200/mogobiz/product/_search"
+
+esquery={
+    "_source": {
+        "exclude": [
+            "*en",
+            "*fr",
+            "*es"
+        ]
+    },
+    "query": {
+        "filtered": {
+            "query": {
+                "match": {
+                    "name": {
+                        "query": "pull tshirt",
+                        "operator": "and"
+                    }
+                }
+            },
+            "filter": {
+                "bool": {
+                    "must": [
+                        {
+                            "range": {
+                                "startFeatureDate": {
+                                    "lte": "2014-02-01"
+                                }
+                            }
+                        },{
+                            "range": {
+                                "endFeatureDate": {
+                                    "gte": "2014-02-01"
+                                }
+                            }
+                        }
+                        ,{
+                            "term": {
+                                "code": "pack_pull_tshirt"
+                            }
+                        },{
+                            "term": {
+                                "category.id": 17
+                            }
+                        }
+                        ,{
+                            "term": {
+                                "xtype": "PACKAGE"
+                            }
+                        }
+                        ,
+                        {
+                            "term": {
+                                "path": "habillage"
+                            }
+                        },
+                        {
+                            "term": {
+                                "brand.id": 28
+                            }
+                        },
+                        {
+                            "term": {
+                                "tags.name": "pull"
+                            }
+                        },
+                        {
+                            "range": {
+                                "price": {
+                                    "gte": 500,
+                                    "lte": 2000
+                                }
+                            }
+                        },
+                        {
+                            "range": {
+                                "imported": {
+                                    "from": "2013-12-31"
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    "sort": {"name": "desc"},
+    "from": 0, //pageOffset * maxItemsPerPage
+    "size": 3 //maxItemsPerPage
+}
--- es-queries/products.js
+++ es-queries/products.js
@@ -1,3 +1,4 @@
+//Statut OK
 // StoreController
 restpath = "/api/store/{storeCode}/products?lang=fr&currency={curCode}&country={countryCode}"
 method = "POST" // et/ou "GET"
@@ -17,18 +18,67 @@ method = "POST" // et/ou "GET"
 method="POST"
 curl = "http://localhost:9200/mogobiz/product/_search"
 
-query = {
+// remarque sur le exclude:
+// 1. permet de virer toute les langues non voulu mais il faut les lister dans la query => donc connaitre la liste des lang sur le store
+// 2. TODO revoir le mapping car le pattern du exclude peut virer des champs finissant par la code_lang. => utiliser un mapping trad.<code_land> :{ nested_objects }
+
+esquery={
+    "_source": {
+        "exclude": [
+            "*en",
+            "*fr",
+            "*es"
+        ]
+    },
     "query": {
         "filtered": {
             "query": {
+                "match": {
+                    "name": {
+                        "query": "pull tshirt",
+                        "operator": "and"
+                    }
+                }
+            },
+            "filter": {
                 "bool": {
                     "must": [
-                        {
+                        /* inutile {
                             "range": {
                                 "startDate": {
                                     "from": "2013-12-31",
                                     "to": "2014-02-01"
                                 }
+                            }
+                        }
+                        ,*/{
+                            "term": {
+                                "code": "pack_pull_tshirt"
+                            }
+                        },{
+                            "term": {
+                                "category.id": 17
+                            }
+                        }
+                        ,{
+                            "term": {
+                                "xtype": "PACKAGE"
+                            }
+                        }
+                        ,
+                        {
+                            "term": {
+                                "path": "habillage"
+                            }
+                        },
+                        {
+                            "term": {
+                                "brand.id": 28
+                            }
+                        },
+                        {
+                            "term": {
+                                "tags.name": "pull"
                             }
                         },
                         {
@@ -38,22 +88,20 @@ query = {
                                     "lte": 2000
                                 }
                             }
-                        },
+                        }/* createDateMin mais pas sûr,
                         {
                             "range": {
                                 "imported": {
                                     "from": "2013-12-31"
                                 }
                             }
-                        }
+                        }*/
                     ]
                 }
             }
         }
     },
-    "sort": {
-        "price": "desc"
-    },
-    "from": 1,
-    "size": 2
+    "sort": {"name": "desc"},
+    "from": 0, //pageOffset * maxItemsPerPage
+    "size": 3 //maxItemsPerPage
 }
