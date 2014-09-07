ec6671bcf3e5e9435b34d4286dfa967368c1c730
cgalant <christophe.galant@gmail.com>
2014-09-08 00:30:49 +0200

    use the right deserialiser for cart types (JodaTime & enumType)

--- src/main/scala/com/mogobiz/services/CartService.scala
+++ src/main/scala/com/mogobiz/services/CartService.scala
@@ -2,11 +2,9 @@ package com.mogobiz.services
 
 import akka.actor.ActorRef
 import com.mogobiz._
-import com.mogobiz.config.JsonSupport._
 import com.mogobiz.actors.CartActor._
 import com.mogobiz.cart.{AddToCartCommand, UpdateCartItemCommand}
-import com.mogobiz.config.JsonSupport._
-import org.json4s.JsonAST.JValue
+import Json4sProtocol._
 import spray.routing.Directives
 
 import scala.concurrent.ExecutionContext
