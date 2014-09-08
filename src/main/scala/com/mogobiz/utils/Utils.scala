d8148943dab37e8f8e6a02a3438aaa53471edb5d
Stéphane Manciot <stephane.manciot@ebiznext.com>
2014-09-08 09:54:29 +0200

    merge Utils

--- src/main/scala/com/mogobiz/utils/Utils.scala
+++ src/main/scala/com/mogobiz/utils/Utils.scala
@@ -1,7 +1,8 @@
 package com.mogobiz.utils
 
-import com.mogobiz.cart.{ProductCalendar, TicketType}
-import org.joda.time.{DateTime, LocalTime}
+import com.mogobiz.cart.ProductCalendar
+import com.mogobiz.cart.domain.TicketType
+import org.joda.time.{LocalTime, DateTime}
 import scalikejdbc._
 
 /**
@@ -11,10 +12,10 @@ import scalikejdbc._
 object Utils {
 
   def printJSON(o:Object)={
-    import org.json4s.JsonDSL._
-    import org.json4s.native.JsonMethods._
-    import org.json4s.native.Serialization._
     import org.json4s.{DefaultFormats, Formats}
+    import org.json4s.native.Serialization._
+    import org.json4s.native.JsonMethods._
+    import org.json4s.JsonDSL._
 
     implicit def json4sFormats: Formats = DefaultFormats
     println("-----------------------------------------------------------------------------------------------")
@@ -41,63 +42,63 @@ object Utils {
     }else{
 
 
-    val ticketType = optTicketType.get
-    val product = ticketType.product.get
-    val date = optDate.get
-    val dateWithTimeReset = date.withTimeAtStartOfDay()
-    val start = ticketType.startDate.getOrElse(DateTime.now().withTimeAtStartOfDay)
-    val stop = ticketType.stopDate.getOrElse(DateTime.now().withTimeAtStartOfDay)
-    val dateValidForTicketType = (dateWithTimeReset.isAfter(start) || dateWithTimeReset.isEqual(start)) && (dateWithTimeReset.isBefore(stop) || dateWithTimeReset.isEqual(stop))
+      val ticketType = optTicketType.get
+      val product = ticketType.product.get
+      val date = optDate.get
+      val dateWithTimeReset = date.withTimeAtStartOfDay()
+      val start = ticketType.startDate.getOrElse(DateTime.now().withTimeAtStartOfDay)
+      val stop = ticketType.stopDate.getOrElse(DateTime.now().withTimeAtStartOfDay)
+      val dateValidForTicketType = (dateWithTimeReset.isAfter(start) || dateWithTimeReset.isEqual(start)) && (dateWithTimeReset.isBefore(stop) || dateWithTimeReset.isEqual(stop))
 
-    if(!dateValidForTicketType) emptyResult
-    else
+      if(!dateValidForTicketType) emptyResult
+      else
 
       // On controle maintenant la date avec le calendrier du produit
-      product.calendarType match {
-        case ProductCalendar.NO_DATE => emptyResult  // Pas de calendrier donc pas de date
+        product.calendarType match {
+          case ProductCalendar.NO_DATE => emptyResult  // Pas de calendrier donc pas de date
         case ProductCalendar.DATE_ONLY =>
 
-          // Calendrier jour seulement, la date doit être comprise entres les dates du produits
-          val dateonly = date.toLocalDate
-          val startDateOnly = product.startDate.get.toLocalDate
-          val stopDateOnly = product.stopDate.get.toLocalDate
-          if ((dateonly.isAfter(startDateOnly) || dateonly.isEqual(startDateOnly)) && (dateonly.isBefore(stopDateOnly) || dateonly.isEqual(stopDateOnly))) {
-            (optDate, optDate)
-          }
-          else {
-            emptyResult
-          }
+            // Calendrier jour seulement, la date doit être comprise entres les dates du produits
+            val dateonly = date.toLocalDate
+            val startDateOnly = product.startDate.get.toLocalDate
+            val stopDateOnly = product.stopDate.get.toLocalDate
+            if ((dateonly.isAfter(startDateOnly) || dateonly.isEqual(startDateOnly)) && (dateonly.isBefore(stopDateOnly) || dateonly.isEqual(stopDateOnly))) {
+              (optDate, optDate)
+            }
+            else {
+              emptyResult
+            }
         case ProductCalendar.DATE_TIME =>
 
-          // on récupère le créneau horraire qui correspond à l'interval pour la date/heure donnée
-          val listIncluded:Seq[IntraDayPeriod] = DB readOnly { implicit session =>
-            //FIXME pb de précision sur les dates :(
-            sql"select * from intra_day_period where product_fk = ${product.id} and start_date <= ${date.plusSeconds(1)} and end_date >= ${date.minusSeconds(1)}"
-              .map(rs => IntraDayPeriod(startDate = rs.get("start_date"), endDate = rs.get("end_date"))).list.apply
-          }
-          val timeonly = date.toLocalTime
-          val res = listIncluded.filter{
-            intraDayPeriod => {
-              val startDateTime = intraDayPeriod.startDate.toLocalTime
-              val endDateTime = intraDayPeriod.endDate.toLocalTime
-              val rightStartDateTime = new LocalTime(startDateTime.getHourOfDay, startDateTime.minuteOfHour().get())
-              val rightEndDateTime = new LocalTime(endDateTime.getHourOfDay, endDateTime.minuteOfHour().get())
-              (timeonly.isAfter(rightStartDateTime)|| timeonly.isEqual(rightStartDateTime)) && (timeonly.isBefore(rightEndDateTime) || timeonly.isEqual(rightEndDateTime))
+            // on récupère le créneau horraire qui correspond à l'interval pour la date/heure donnée
+            val listIncluded:Seq[IntraDayPeriod] = DB readOnly { implicit session =>
+              //FIXME pb de précision sur les dates :(
+              sql"select * from intra_day_period where product_fk = ${product.id} and start_date <= ${date.plusSeconds(1)} and end_date >= ${date.minusSeconds(1)}"
+                .map(rs => IntraDayPeriod(startDate = rs.get("start_date"), endDate = rs.get("end_date"))).list.apply
+            }
+            val timeonly = date.toLocalTime
+            val res = listIncluded.filter{
+              intraDayPeriod => {
+                val startDateTime = intraDayPeriod.startDate.toLocalTime
+                val endDateTime = intraDayPeriod.endDate.toLocalTime
+                val rightStartDateTime = new LocalTime(startDateTime.getHourOfDay, startDateTime.minuteOfHour().get())
+                val rightEndDateTime = new LocalTime(endDateTime.getHourOfDay, endDateTime.minuteOfHour().get())
+                (timeonly.isAfter(rightStartDateTime)|| timeonly.isEqual(rightStartDateTime)) && (timeonly.isBefore(rightEndDateTime) || timeonly.isEqual(rightEndDateTime))
+              }
+            }
+            // et on renvoie le créneau horaire
+            res.headOption match{
+            case Some(idp) =>
+                //DateTime(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour)
+                val startDate = new DateTime(date.getYear, date.getMonthOfYear, date.dayOfMonth().get(), idp.startDate.hourOfDay().get(), idp.startDate.minuteOfHour().get())
+                val endDate = new DateTime(date.getYear, date.getMonthOfYear, date.dayOfMonth().get(), idp.endDate.hourOfDay().get(), idp.endDate.minuteOfHour().get())
+
+                (Some(startDate),Some(endDate))
+              case _ => emptyResult
             }
           }
-          // et on renvoie le créneau horaire
-          res.headOption match{
-            case Some(idp) =>
-              //DateTime(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour)
-              val startDate = new DateTime(date.getYear, date.getMonthOfYear, date.dayOfMonth().get(), idp.startDate.hourOfDay().get(), idp.startDate.minuteOfHour().get())
-              val endDate = new DateTime(date.getYear, date.getMonthOfYear, date.dayOfMonth().get(), idp.endDate.hourOfDay().get(), idp.endDate.minuteOfHour().get())
-
-              (Some(startDate),Some(endDate))
-            case _ => emptyResult
-          }
-      }
+        }
     }
-  }
   case class IntraDayPeriod(startDate:DateTime, endDate:DateTime)
 
 }
@@ -119,8 +120,8 @@ trait Copying[T] {
         case None =>
           val getter = clas.getMethod(fieldName)
           getter.invoke(this)
+        }
       }
-    }
     constructor.newInstance(arguments: _*).asInstanceOf[T]
   }
 }
