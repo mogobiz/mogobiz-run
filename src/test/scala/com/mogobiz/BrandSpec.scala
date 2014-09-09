42bb4d45d10a0d356074c01e67c9e5abd516892e
Yoann Baudy <yoann.baudy@ebiznext.com>
2014-09-10 00:52:11 +0200

    TU Brands

--- src/test/scala/com/mogobiz/BrandSpec.scala
+++ src/test/scala/com/mogobiz/BrandSpec.scala
@@ -7,8 +7,9 @@ import spray.routing.HttpService
 import com.mogobiz.services.MogobizRoutes
 import com.mogobiz.actors.{MogobizActors, MogobizSystem}
 import scala.concurrent.duration._
-import org.specs2.matcher.JsonMatchers
-
+import org.specs2.matcher._
+import org.json4s.native.JsonParser
+import org.json4s.JsonAST._
 
 /**
  *
@@ -22,12 +23,112 @@ class BrandSpec extends Specification with Specs2RouteTest with HttpService with
 
   step(start)
 
+  def sortById(j: JValue): List[JValue] = {
+    j match {
+      case JArray(a) => a.sortWith((v1: JValue, v2: JValue) => {
+                          (v1 \ "id", v2 \ "id") match {case (JInt(id1), JInt(id2)) => id1 < id2}
+                        })
+      case JObject(v) => List(JObject(v))
+    }
+  }
+
   "The Brand service" should {
-    "return all brands" in {
+    "return not hidden brands" in {
       Get("/store/" + STORE + "/brands") ~> routes ~> check {
-        val r : String = responseAs[String]
-        r must /#(0) / ("id" -> 40)
+        val brands = sortById(JsonParser.parse(responseAs[String]))
+        brands must have size(5)
+        checkBrandSamsung(brands(0))
+        checkBrandSamsung(brands(1))
+        checkBrandPhilips(brands(2))
+        checkBrandNike(brands(3))
+        checkBrandPuma(brands(4))
       }
     }
+
+    "return not hidden brands" in {
+      Get("/store/" + STORE + "/brands?hidden=true") ~> routes ~> check {
+        val brands = sortById(JsonParser.parse(responseAs[String]))
+        brands must have size(6)
+        checkBrandSamsung(brands(0))
+        checkBrandSamsung(brands(1))
+        checkBrandPhilips(brands(2))
+        checkBrandNike(brands(3))
+        checkBrandPuma(brands(4))
+        checkBrandHideBrand(brands(5))
+      }
+    }
+  }
+
+  def checkBrandSamsung(brand: JValue) : MatchResult[JValue] = {
+    brand \ "id" must be_==(JInt(35))
+    brand \ "twitter" must be_==(JNull)
+    brand \ "hide" must be_==(JBool(false))
+    brand \ "website" must be_==(JString("http://www.samsung.com/fr"))
+    brand \ "description" must be_==(JNull)
+    brand \ "name" must be_==(JString("Samsung"))
+    brand \ "increments" must be_==(JInt(0))
+    brand \ "imported" must be_==(JNothing)
+    brand \ "fr" \ "name" must be_==(JString("Samsung"))
+    brand \ "fr" \ "website" must be_==(JString("http://www.samsung.com/fr"))
+    brand \ "en" \ "website" must be_==(JString("http://www.samsung.com"))
+    brand \ "es" \ "website" must be_==(JString("http://www.samsung.com/es"))
+  }
+
+  def checkBrandPhilips(brand: JValue) : MatchResult[JValue] = {
+    brand \ "id" must be_==(JInt(40))
+    brand \ "twitter" must be_==(JNull)
+    brand \ "hide" must be_==(JBool(false))
+    brand \ "website" must be_==(JString("http://www.philips.com"))
+    brand \ "description" must be_==(JNull)
+    brand \ "name" must be_==(JString("Philips"))
+    brand \ "increments" must be_==(JInt(0))
+    brand \ "imported" must be_==(JNothing)
+    brand \ "fr" \ "name" must be_==(JString("Philips"))
+    brand \ "fr" \ "website" must be_==(JString("http://www.philips.com"))
+  }
+
+  def checkBrandNike(brand: JValue) : MatchResult[JValue] = {
+    brand \ "id" must be_==(JInt(41))
+    brand \ "twitter" must be_==(JNull)
+    brand \ "hide" must be_==(JBool(false))
+    brand \ "website" must be_==(JString("http://www.nike.com/fr/fr_fr/"))
+    brand \ "description" must be_==(JNull)
+    brand \ "name" must be_==(JString("Nike"))
+    brand \ "increments" must be_==(JInt(0))
+    brand \ "imported" must be_==(JNothing)
+    brand \ "fr" \ "name" must be_==(JString("Nike"))
+    brand \ "fr" \ "website" must be_==(JString("http://www.nike.com/fr/fr_fr/"))
+    brand \ "de" \ "website" must be_==(JString("http://www.nike.com/de/de_de/"))
+    brand \ "en" \ "website" must be_==(JString("http://www.nike.com"))
+    brand \ "es" \ "website" must be_==(JString("http://www.nike.com/es/es_es/"))
+  }
+
+  def checkBrandPuma(brand: JValue) : MatchResult[JValue] = {
+    brand \ "id" must be_==(JInt(46))
+    brand \ "twitter" must be_==(JNull)
+    brand \ "hide" must be_==(JBool(false))
+    brand \ "website" must be_==(JString("http://www.shop.puma.fr"))
+    brand \ "description" must be_==(JNull)
+    brand \ "name" must be_==(JString("Puma"))
+    brand \ "increments" must be_==(JInt(0))
+    brand \ "imported" must be_==(JNothing)
+    brand \ "fr" \ "name" must be_==(JString("Puma"))
+    brand \ "fr" \ "website" must be_==(JString("http://www.shop.puma.fr"))
+    brand \ "de" \ "website" must be_==(JString("http://www.shop.puma.de"))
+    brand \ "en" \ "website" must be_==(JString("http://www.puma.com"))
+    brand \ "es" \ "website" must be_==(JString("http://www.puma.com"))
+  }
+
+  def checkBrandHideBrand(brand: JValue) : MatchResult[JValue] = {
+    brand \ "id" must be_==(JInt(51))
+    brand \ "twitter" must be_==(JNull)
+    brand \ "hide" must be_==(JBool(true))
+    brand \ "website" must be_==(JString("http://www.google.fr"))
+    brand \ "description" must be_==(JNull)
+    brand \ "name" must be_==(JString("Hide brand"))
+    brand \ "increments" must be_==(JInt(0))
+    brand \ "imported" must be_==(JNothing)
+    brand \ "fr" \ "name" must be_==(JString("Hide brand"))
+    brand \ "fr" \ "website" must be_==(JString("http://www.google.fr"))
   }
 }
