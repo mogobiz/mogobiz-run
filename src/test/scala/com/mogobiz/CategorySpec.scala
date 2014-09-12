6f40ac32170ac8c9022fcc6e19869c17e888cec0
Yoann Baudy <yoann.baudy@ebiznext.com>
2014-09-12 22:17:44 +0200

    Remis à jour des data d’ES + correction TU suite au changement de données

--- src/test/scala/com/mogobiz/CategorySpec.scala
+++ src/test/scala/com/mogobiz/CategorySpec.scala
@@ -102,7 +102,7 @@ class CategorySpec extends MogobizRouteTest {
     category \ "name" must be_==(JString("Hightech"))
     category \ "path" must be_==(JString("hightech"))
     category \ "increments" must be_==(JInt(0))
-    category \ "uuid" must be_==(JString("71eabed1-b6e2-4066-a945-5963215d0378"))
+    category \ "uuid" must be_==(JString("0a002eaa-8744-4992-af4a-be6a0d764741"))
     category \ "imported" must be_==(JNothing)
     checkLang(category, checkOnlyLang, "fr", JString("Hightech"), JNull, JNull)
   }
@@ -116,7 +116,7 @@ class CategorySpec extends MogobizRouteTest {
     category \ "name" must be_==(JString("Habillement"))
     category \ "path" must be_==(JString("habillement"))
     category \ "increments" must be_==(JInt(0))
-    category \ "uuid" must be_==(JString("f28f40fc-0362-4f21-9f26-7de561c497fa"))
+    category \ "uuid" must be_==(JString("5fe14b9b-4e55-40db-8ace-cfc77a1b1db1"))
     category \ "imported" must be_==(JNothing)
     checkLang(category, checkOnlyLang, "fr", JString("Habillement"), JNull, JString("vetements homme femme enfant"))
   }
@@ -130,7 +130,7 @@ class CategorySpec extends MogobizRouteTest {
     category \ "name" must be_==(JString("Télévisions"))
     category \ "path" must be_==(JString("hightech/televisions"))
     category \ "increments" must be_==(JInt(0))
-    category \ "uuid" must be_==(JString("24c453b0-f407-4156-a507-021779959d1a"))
+    category \ "uuid" must be_==(JString("77057fd0-0709-4320-a14e-2957bc96946c"))
     category \ "imported" must be_==(JNothing)
     checkLang(category, checkOnlyLang, "fr", JString("Télévisions"), JNull, JString("TV télé télévision HD"))
     checkLang(category, checkOnlyLang, "en", JString("Televisions"), JNothing, JNothing)
@@ -145,7 +145,7 @@ class CategorySpec extends MogobizRouteTest {
     category \ "name" must be_==(JString("Cinéma"))
     category \ "path" must be_==(JString("cinema"))
     category \ "increments" must be_==(JInt(0))
-    category \ "uuid" must be_==(JString("4c1dcd08-80b3-476b-9b45-64c0718f674b"))
+    category \ "uuid" must be_==(JString("a9cd16e3-6f89-4311-9e57-123b0c5bfec5"))
     category \ "imported" must be_==(JNothing)
     checkLang(category, checkOnlyLang, "fr", JString("Cinéma"), JNull, JNull)
   }
