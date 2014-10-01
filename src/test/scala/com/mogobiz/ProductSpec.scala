a914502aba97c43e4c2dcfc9b8c040703a9fbb0c
Stéphane Manciot <stephane.manciot@ebiznext.com>
2014-10-01 09:51:00 +0200

    initiate Product route specification

---
+++ src/test/scala/com/mogobiz/ProductSpec.scala
@@ -0,0 +1,24 @@
+package com.mogobiz
+
+import org.specs2.matcher._
+import org.json4s.native.JsonParser
+import org.json4s.JsonAST._
+
+/**
+ *
+ * Created by smanciot on 19/09/14.
+ */
+class ProductSpec extends MogobizRouteTest{
+
+  "The product service" should {
+
+    "return products, categories, brands and tags for suggestions" in {
+      Get("/store/" + STORE + "/products/find?query=hab") ~> sealRoute(routes) ~> check {
+        val suggestions: List[JValue] = checkJArray(JsonParser.parse(responseAs[String]))
+        suggestions must not be null
+      }
+    }
+
+  }
+
+}
