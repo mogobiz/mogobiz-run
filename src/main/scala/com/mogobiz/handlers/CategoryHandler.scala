0afd01cf08779a676bdfddb984d465a712f7a14d
Stéphane Manciot <stephane.manciot@ebiznext.com>
2014-10-13 11:44:13 +0200

    to fix JAHIAC-61

--- src/main/scala/com/mogobiz/handlers/CategoryHandler.scala
+++ src/main/scala/com/mogobiz/handlers/CategoryHandler.scala
@@ -3,27 +3,31 @@ package com.mogobiz.handlers
 import com.mogobiz.es._
 import com.mogobiz.es.EsClient
 import com.mogobiz.es.EsClient._
+import com.mogobiz.json.JsonUtil
 import com.sksamuel.elastic4s.ElasticDsl.{search => esearch4s, _}
 import com.sksamuel.elastic4s.FilterDefinition
 import org.json4s.JsonAST.JValue
+import org.json4s.JsonDSL._
 import org.json4s._
 
-class CategoryHandler {
+class CategoryHandler extends JsonUtil {
 
   def queryCategories(store: String, hidden: Boolean, parentId: Option[String], brandId: Option[String], categoryPath: Option[String], lang: String, promotionId: Option[String]): JValue = {
-    var filters:List[FilterDefinition] = if(!hidden) List(termFilter("category.hide", "false")) else List.empty
+    var filters:List[FilterDefinition] = List.empty
     def results(req:SearchDefinition):JValue = EsClient.searchAllRaw(filterRequest(req, filters) sourceExclude(createExcludeLang(store, lang) :+ "imported" :_*)).getHits
     brandId match {
       case Some(s) =>
         filters +:= termFilter("brand.id", s)
+        if(!hidden) filters +:= termFilter("category.hide", "false")
         if(parentId.isDefined) filters +:= termFilter("category.parentId", parentId.get)
         if(categoryPath.isDefined) filters +:= createRegexFilter("category.path", categoryPath).get
         if(promotionId.isDefined) filters +:= createTermFilter("category.coupons", promotionId).get
-        results(esearch4s in store -> "product") \ "category"
+        distinctById(results(esearch4s in store -> "product") \ "category")
       case None =>
+        if(!hidden) filters +:= termFilter("hide", "false")
         if(parentId.isDefined) filters +:= termFilter("parentId", parentId.get)
-        else if(categoryPath.isEmpty) missingFilter("parentId") existence true includeNull true
         if(categoryPath.isDefined) filters +:= createRegexFilter("path", categoryPath).get
+        else if(!parentId.isDefined) filters +:= missingFilter("parentId") existence true includeNull true
         if(promotionId.isDefined) filters +:= createTermFilter("coupons", promotionId).get
         results(esearch4s in store -> "category")
     }
