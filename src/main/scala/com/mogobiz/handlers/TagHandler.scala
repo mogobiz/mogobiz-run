69298d57b3eea17a37d867e7cbef1e090022138f
Stéphane Manciot <stephane.manciot@ebiznext.com>
2014-10-23 12:01:12 +0200

    excluding instead of including

--- src/main/scala/com/mogobiz/handlers/TagHandler.scala
+++ src/main/scala/com/mogobiz/handlers/TagHandler.scala
@@ -1,6 +1,6 @@
 package com.mogobiz.handlers
 
-import com.mogobiz.es.EsClient
+import com.mogobiz.es._
 import com.mogobiz.es.EsClient._
 import com.sksamuel.elastic4s.ElasticDsl.{search => esearch4s, _}
 import org.json4s.JsonAST.JValue
@@ -10,7 +10,7 @@ class TagHandler {
 
   def queryTags(storeCode: String, hidden: Boolean, inactive: Boolean, lang: String): JValue = {
     EsClient.searchAllRaw(
-      esearch4s in storeCode -> "tag" sourceInclude("id", if (lang == "_all") "*.*" else s"$lang.*")
+      esearch4s in storeCode -> "tag" sourceExclude(createExcludeLang(storeCode, lang) :+ "imported" :_*)
     ).getHits
   }
 
