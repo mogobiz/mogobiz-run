199dfe332b1e48ab1067ec622f2858784ff00dcc
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-10-29 16:27:08 +0100

    minor change

--- src/main/scala/com/mogobiz/services/WishlistService.scala
+++ src/main/scala/com/mogobiz/services/WishlistService.scala
@@ -1,6 +1,6 @@
 package com.mogobiz.services
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef}
 import akka.pattern.ask
 import akka.util.Timeout
 import com.mogobiz.actors.WishlistActor._
