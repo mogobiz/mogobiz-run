d4e54049fe2876670bf001c4ababa356a22ebd0c
cgalant <christophe.galant@gmail.com>
2014-11-18 23:29:44 +0100

    [REFACTO] Country

---
+++ src/test/scala/com/mogobiz/services/CountryServiceSpec.scala
@@ -0,0 +1,18 @@
+package com.mogobiz.services
+
+import com.mogobiz.MogobizRouteTest
+import org.specs2.matcher._
+import org.json4s.native.JsonParser
+import org.json4s.JsonAST._
+
+class CountryServiceSpec extends MogobizRouteTest {
+  "The Country service" should {
+
+    "return countries" in {
+      Get("/store/" + STORE + "/countries") ~> sealRoute(routes) ~> check {
+        val countries: List[JValue] = checkJArray(JsonParser.parse(responseAs[String]))
+        countries must have size 0
+      }
+    }
+  }
+}
