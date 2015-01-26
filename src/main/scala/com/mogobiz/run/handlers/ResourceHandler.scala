ddc7b4fc02419750c1b0d92cac856e0a8a705e77
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-01-27 00:54:17 +0100

    load resources from es if necessary

--- src/main/scala/com/mogobiz/run/handlers/ResourceHandler.scala
+++ src/main/scala/com/mogobiz/run/handlers/ResourceHandler.scala
@@ -4,18 +4,36 @@ import com.mogobiz.run.config.Settings
 import Settings._
 import com.mogobiz.utils.ImageUtils
 import com.mogobiz.utils.ImageUtils._
-import java.io.File
+import java.io.{FileOutputStream, File}
+
+import com.mogobiz.es.EsClient._
+import com.sksamuel.elastic4s.ElasticDsl._
+import org.elasticsearch.common.bytes.ChannelBufferBytesReference
 
 class ResourceHandler {
 
   def queryResource(storeCode: String, resourceId:String, size:Option[String]): Option[String] = {
-    val file = new File(s"$ResourcesRootPath/resources/$storeCode/image/$resourceId")
+    val dir = s"$ResourcesRootPath/resources/$storeCode/image"
+    val path = s"$dir/$resourceId"
+    val file = new File(path)
+    if(!file.exists()){
+      loadRaw(get id resourceId from storeCode -> "resource" fields "content" fetchSourceContext true) match {
+        case Some(response) =>
+          if(response.getFields.containsKey("content")){
+            val content = response.getField("content").getValue.asInstanceOf[ChannelBufferBytesReference]
+            if(new File(dir).mkdirs()){
+              content.writeTo(new FileOutputStream(file))
+            }
+          }
+        case _ =>
+      }
+    }
     if(file.exists()){
       size match {
         case Some(s) =>
           Some(ImageUtils.getFile(file, imageSizes.get(s.toUpperCase), create = true).getAbsolutePath)
         case _ =>
-          Some(file.getAbsolutePath)
+          Some(path)
       }
     }
     else{
