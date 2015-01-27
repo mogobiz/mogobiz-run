b599a6296e437617b63aa0ce5ab453952756f537
Yoann Baudy <yoann.baudy@ebiznext.com>
2015-01-27 17:46:21 +0100

    JAHIAC-201

--- src/main/scala/com/mogobiz/run/model/Learning.scala
+++ src/main/scala/com/mogobiz/run/model/Learning.scala
@@ -1,5 +1,8 @@
 package com.mogobiz.run.model
 
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
+
 
 object UserAction extends Enumeration {
   type UserAction = Value
@@ -7,11 +10,17 @@ object UserAction extends Enumeration {
   val View = Value("view")
 }
 
+class UserActionRef extends TypeReference[UserAction.type]
+
 import java.util.Calendar
 import java.util.Date
 
 import UserAction._
 
-case class UserItemAction(val trackingid: String, val itemid: String, action: UserAction, timestamp: Date = Calendar.getInstance().getTime)
+case class UserItemAction(val trackingid: String,
+                          val itemid: String,
+                          @JsonScalaEnumeration(classOf[UserActionRef])
+                          action: UserAction,
+                          timestamp: Date = Calendar.getInstance().getTime)
 
 case class CartAction(val trackingid: String, val itemids: String, timestamp: Date = Calendar.getInstance().getTime)
