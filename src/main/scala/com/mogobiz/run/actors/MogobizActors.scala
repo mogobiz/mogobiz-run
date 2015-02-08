8dfd966c5623888dd7b8e698f123f7c5d088b914
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-02-08 21:39:46 +0100

    external router

--- src/main/scala/com/mogobiz/run/actors/MogobizActors.scala
+++ src/main/scala/com/mogobiz/run/actors/MogobizActors.scala
@@ -1,27 +1,22 @@
 package com.mogobiz.run.actors
 
 import akka.actor.Props
+import akka.routing.FromConfig
 import com.mogobiz.system.MogobizSystem
 
-
-/**
- * This trait contains the actors that make up our application; it can be mixed in with
- * ``BootedCore`` for running code or ``TestKit`` for unit and integration tests.
- */
 trait MogobizActors {
   this: MogobizSystem =>
-
-  val tagActor = system.actorOf(Props[TagActor])
-  val brandActor = system.actorOf(Props[BrandActor])
-  val langActor = system.actorOf(Props[LangActor])
-  val countriesActor = system.actorOf(Props[CountryActor])
-  val currencyActor = system.actorOf(Props[CurrencyActor])
-  val categoryActor = system.actorOf(Props[CategoryActor])
-  val productActor = system.actorOf(Props[ProductActor])
-  val preferenceActor = system.actorOf(Props[PreferenceActor])
-  val cartActor = system.actorOf(Props[CartActor])
-  val promotionActor = system.actorOf(Props[PromotionActor])
-  val wishlistActor =system.actorOf(Props[WishlistActor])
-  val facetActor = system.actorOf(Props[FacetActor])
+  val tagActor = system.actorOf(Props[TagActor].withRouter(FromConfig()), "tag")
+  val brandActor = system.actorOf(Props[BrandActor].withRouter(FromConfig()), "brand")
+  val langActor = system.actorOf(Props[LangActor].withRouter(FromConfig()), "lang")
+  val countriesActor = system.actorOf(Props[CountryActor].withRouter(FromConfig()), "countries")
+  val currencyActor = system.actorOf(Props[CurrencyActor].withRouter(FromConfig()), "currency")
+  val categoryActor = system.actorOf(Props[CategoryActor].withRouter(FromConfig()), "category")
+  val productActor = system.actorOf(Props[ProductActor].withRouter(FromConfig()), "product")
+  val preferenceActor = system.actorOf(Props[PreferenceActor].withRouter(FromConfig()), "preference")
+  val cartActor = system.actorOf(Props[CartActor].withRouter(FromConfig()), "cart")
+  val promotionActor = system.actorOf(Props[PromotionActor].withRouter(FromConfig()), "promotion")
+  val wishlistActor = system.actorOf(Props[WishlistActor].withRouter(FromConfig()), "wishlist")
+  val facetActor = system.actorOf(Props[FacetActor].withRouter(FromConfig()), "facet")
 }
 
