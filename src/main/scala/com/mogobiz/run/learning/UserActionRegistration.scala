3a012d47b6f2b34a45ad93a55ae54a862a0eb7a6
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-02-16 16:37:39 +0100

    Include Learning API

--- src/main/scala/com/mogobiz/run/learning/UserActionRegistration.scala
+++ src/main/scala/com/mogobiz/run/learning/UserActionRegistration.scala
@@ -1,14 +1,14 @@
 package com.mogobiz.run.learning
 
 import com.mogobiz.es.EsClient
-import com.mogobiz.run.model.UserAction._
-import com.mogobiz.run.model.UserItemAction
+import com.mogobiz.run.model.MogoLearn.UserAction.UserAction
+import com.mogobiz.run.model.MogoLearn.UserItemAction
 
 
 object UserActionRegistration {
   def esStore(store: String): String = s"${store}_learning"
 
   def register(store: String, trackingid: String, itemid: String, action: UserAction): String = {
-    EsClient.index(esStore(store), UserItemAction(trackingid, itemid, action))
+    EsClient.index(esStore(store), UserItemAction(trackingid, itemid, action), false)
   }
 }
--- src/main/scala/com/mogobiz/run/model/Learning.scala
+++ src/main/scala/com/mogobiz/run/model/Learning.scala
@@ -3,24 +3,44 @@ package com.mogobiz.run.model
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
 
+object MogoLearn {
+  def esStore(store: String): String = s"${store}_predictions"
 
-object UserAction extends Enumeration {
-  type UserAction = Value
-  val Purchase = Value("purchase")
-  val View = Value("view")
+  def esInputStore(store: String): String = s"${store}_learning"
+
+  object UserAction extends Enumeration {
+    type UserAction = Value
+    val Purchase = Value("purchase")
+    val View = Value("view")
+  }
+
+  class UserActionRef extends TypeReference[UserAction.type]
+
+  import java.util.Calendar
+  import java.util.Date
+
+  import UserAction._
+
+  case class UserItemAction(val uuid: String,
+                            val itemid: String,
+                            @JsonScalaEnumeration(classOf[UserActionRef])
+                            action: UserAction,
+                            var dateCreated: Date = Calendar.getInstance().getTime,
+                            var lastUpdated: Date = Calendar.getInstance().getTime
+                             ) {
+    def toMap(): Map[String, String] = {
+      Map("uuid" -> uuid, "itemid" -> itemid, "action" -> action.toString(), "dateCreated" -> dateCreated.getTime().toString, "lastUpdated" -> lastUpdated.getTime().toString)
+    }
+  }
+
+  case class CartAction(val uuid: String,
+                        val itemids: String,
+                        var dateCreated: Date = Calendar.getInstance().getTime,
+                        var lastUpdated: Date = Calendar.getInstance().getTime) {
+    def toMap(): Map[String, String] = {
+      Map("uuid" -> uuid, "items" -> itemids, "dateCreated" -> dateCreated.getTime().toString, "lastUpdated" -> lastUpdated.getTime().toString)
+    }
+  }
+
+  case class Prediction(uid: String, purchase: List[String], view: List[String], timestamp: Long);
 }
-
-class UserActionRef extends TypeReference[UserAction.type]
-
-import java.util.Calendar
-import java.util.Date
-
-import UserAction._
-
-case class UserItemAction(val trackingid: String,
-                          val itemid: String,
-                          @JsonScalaEnumeration(classOf[UserActionRef])
-                          action: UserAction,
-                          timestamp: Date = Calendar.getInstance().getTime)
-
-case class CartAction(val trackingid: String, val itemids: String, timestamp: Date = Calendar.getInstance().getTime)
