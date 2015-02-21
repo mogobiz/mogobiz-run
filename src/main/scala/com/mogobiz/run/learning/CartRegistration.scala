82230928a9ebcb2899d778d020c830ca7c42deb2
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-02-21 01:50:28 +0100

    cart combinations bulk upsert within learning store

--- src/main/scala/com/mogobiz/run/learning/CartRegistration.scala
+++ src/main/scala/com/mogobiz/run/learning/CartRegistration.scala
@@ -1,25 +1,58 @@
 package com.mogobiz.run.learning
 
+import akka.stream.ActorFlowMaterializer
 import com.mogobiz.es.EsClient
-import com.mogobiz.run.model.MogoLearn.CartAction
+import com.mogobiz.run.model.MogoLearn._
+import com.mogobiz.system.BootedMogobizSystem
+import com.sksamuel.elastic4s.BulkCompatibleDefinition
+import org.elasticsearch.action.bulk.BulkResponse
 
 import scala.collection.mutable.ListBuffer
 
+import akka.stream.scaladsl._
 
-object CartRegistration {
+object CartRegistration extends BootedMogobizSystem {
   def esStore(store: String): String = s"${store}_learning"
 
-  def register(store: String, trackingid: String, itemids: Seq[String]): String = {
-//    var i = 0
-//    val sorted = itemids.map(_.toLong).sorted
-//    val combinations : ListBuffer[Seq[String]] = ListBuffer.empty
-//    for (i <- 1 to sorted.length) {
-//      combinations ++= sorted.combinations(i).map(_.map(_.toString)).toList
-//    }
-//     calcul de toutes les combinaisons à placer dans un acteur
+  val combinationsFlow = Flow(){implicit b =>
+    import FlowGraphImplicits._
 
+    val undefinedSource = UndefinedSource[Seq[Long]]
+    val undefinedSink = UndefinedSink[List[Seq[Long]]]
 
-    EsClient.index(esStore(store), CartAction(trackingid, itemids.mkString(" ")), false)
+    undefinedSource ~> Flow[Seq[Long]].map[List[Seq[Long]]](s => {
+      val combinations : ListBuffer[Seq[Long]] = ListBuffer.empty
+      1 to s.length foreach {i => combinations ++= s.combinations(i)/*.map(_.sorted)*/.toList}
+      combinations.toList
+    }) ~> undefinedSink
+
+    (undefinedSource, undefinedSink)
   }
+
+  def register(store: String, trackingid: String, itemids: Seq[String]): String = {
+    val sorted: Seq[Long] = itemids.map(_.toLong).distinct.sorted
+
+    if(sorted.length > 100){
+      EsClient.index(esStore(store), CartAction(trackingid, itemids.mkString(" ")))
+    }
+    else{
+      implicit val _ = ActorFlowMaterializer()
+      import com.sksamuel.elastic4s.ElasticDsl._
+
+      val bulkFlow = Flow[List[Seq[Long]]].map(_.map(seq =>
+        update(seq.mkString("-"))
+          .in(s"${esStore(store)}/CartCombination")
+          .upsert("combinations" -> seq.map(_.toString), "counter" -> 1)
+          .script("ctx._source.counter += count")
+          .params("count" -> 1)
+      )).mapConcat[BulkCompatibleDefinition](identity).grouped(100).map(EsClient.bulk(_))
+
+      val sink = ForeachSink[BulkResponse](resp => println(s"${resp.getItems.length} -> ${resp.getTookInMillis} ms"))
+
+      combinationsFlow.via(bulkFlow).runWith(Source.single(sorted), sink)
+      ""
+    }
+  }
+
 }
 
