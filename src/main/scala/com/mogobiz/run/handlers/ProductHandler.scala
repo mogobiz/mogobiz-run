4b33d01e424085ef56936832db094d35c730c1b8
cgalant <christophe.galant@gmail.com>
2015-07-06 01:19:07 +0200

    [JAHIAC-288] filter products on VAT price when queryparam country=<code>

--- src/main/scala/com/mogobiz/run/handlers/ProductHandler.scala
+++ src/main/scala/com/mogobiz/run/handlers/ProductHandler.scala
@@ -56,6 +56,10 @@ class ProductHandler extends JsonUtil {
     }
 
     val lang = if (productRequest.lang == "_all") "" else s"${productRequest.lang}."
+    val priceWithVatField = productRequest.countryCode match{
+      case Some(countryCode) => s"${countryCode}.endPrice"
+      case _ => "price"
+    }
 
     val filters: List[FilterDefinition] = List(
       createOrFilterBySplitValues(productRequest.id, v => createTermFilter("id", Some(v))),
@@ -65,7 +69,7 @@ class ProductHandler extends JsonUtil {
       createOrFilterBySplitValues(productRequest.brandId, v => createTermFilter("brand.id", Some(v))),
       createOrFilterBySplitValues(productRequest.tagName.map(_.toLowerCase), v => createNestedTermFilter("tags", "tags.name.raw", Some(v))),
       createOrFilterBySplitValues(productRequest.notations, v => createNestedTermFilter("notations", "notations.notation", Some(v))),
-      createOrFilterBySplitKeyValues(productRequest.priceRange, (min, max) => createNumericRangeFilter("price", min, max)),
+      createOrFilterBySplitKeyValues(productRequest.priceRange, (min, max) => createNumericRangeFilter(s"${priceWithVatField}", min, max)),
       createOrFilterBySplitValues(productRequest.creationDateMin, v => createRangeFilter("dateCreated", Some(v), None)),
       createOrFilterBySplitValues(productRequest.promotionId, v => createTermFilter("coupons.id", Some(v))),
       createFeaturedRangeFilters(productRequest),
