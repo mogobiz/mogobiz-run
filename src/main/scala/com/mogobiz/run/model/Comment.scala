d8573255a37a80db1ba8ea14cd9cf7e0c025822b
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-06-14 21:43:35 +0200

    Initial migration

--- src/main/scala/com/mogobiz/run/model/Comment.scala
+++ src/main/scala/com/mogobiz/run/model/Comment.scala
@@ -6,7 +6,5 @@ package com.mogobiz.run.model
 
 import java.util.Date
 
-import com.mogobiz.run.utils.PagingParams
-
 case class Comment(id: String, userId: String, surname: String, notation: Int, subject: String, comment: String, externalCode: Option[String], created: Date, productId: Long, useful: Int = 0, notuseful: Int = 0)
 
